e6c84e8998f0f375514676d565317bc9e9df1738
Patrick Henkel <patrick.henkel@eonerc.rwth-aachen.de>
2024-06-05 16:45:22 +0200

    Updated to align with newest pull-requests (mainly FileManager)

--- Examples/BopTest/bestest_hydronic_heat_pump/4_mpc.py
+++ Examples/BopTest/bestest_hydronic_heat_pump/4_mpc.py
@@ -1,12 +1,12 @@
 from Examples.BopTest.bestest_hydronic_heat_pump.configuration import *
 
+mpc_name = 'test'
+scenario = 'typical_heat_day'
+
 # regression for TAirRoom
 # TAirRoom_pred: LinearRegression = load_LinearRegression(filename='TairRoom_linReg')
 # TAirRoom_ped: GaussianProcess = load_GaussianProcess(filename='TairRoom_GPR')
 TAirRoom_pred: NeuralNetwork = load_NetworkTrainer(filename="TairRoom_ANN").best
-TAirRoom.mode = Economic()
-
-
 power_hp_wb = WhiteBox(
     inputs=[u_hp.source, t_amb.source, TAirRoom.source, u_hp_logistic.source],
     output=power_hp,
@@ -18,8 +18,10 @@ power_hp_wb = WhiteBox(
     ),
     step_size=one_minute * 15,
 )
+# power_hp_gpr = load_GaussianProcess("power_hp_GPR_500_IP")
 
-power_hp_gpr = load_GaussianProcess("power_hp_GPR_500_IP")
+TAirRoom.mode = TAirRoom_economic
+FileManager.experiment = f'{mpc_name}'
 
 hhp_MPC = ModelPredictive(
     step_size=one_minute * 15,
@@ -27,7 +29,7 @@ hhp_MPC = ModelPredictive(
         model=model,
         N=32,
         objectives=[
-            Objective(feature=TAirRoom, cost=Quadratic(weight=50)),
+            Objective(feature=TAirRoom, cost=Quadratic(weight=10)),
             Objective(feature=costs_el, cost=AbsoluteLinear(weight=1)),
             # Objective(feature=u_hp,             cost=Linear(1)),
             Objective(feature=u_hp_change, cost=Quadratic(weight=0.1)),
@@ -44,50 +46,56 @@ hhp_MPC = ModelPredictive(
 )
 
 system.setup(
-    start_time=one_week * 4,
+    start_time=0,
     warmup_period=one_week,
+    scenario={'electricity_price': 'dynamic',
+              'time_period': scenario},
     active_control_layers={"oveHeaPumY_activate": 1},
 )
 
-dh = load_DataContainer("pid_data")
-
-TAirRoom_TrainingData.add(raw_data=dh)
-power_hp_TrainingData.add(raw_data=dh)
-
 solver_options = {
     "verbose": False,
-    "ipopt.print_level": 1,
-    "warn_initial_bounds": True,
-    # 'ipopt.tol': 1e-5,
-    # 'ipopt.acceptable_tol': 1e-2,
-    # 'ipopt.acceptable_iter': 10,
+    "ipopt.print_level": 2,
+    "ipopt.max_iter": 1000,
+    "expand": True,
+    'ipopt.tol': 1e-1,
+    'ipopt.acceptable_tol': 1,
 }
-system.run(controllers=(), duration=one_day)
 
-# online mpc loop
-for repetition in range(7):
+df = None
+
+for repetition in range(14):
     hhp_MPC.nlp.build(
         solver_options=solver_options, predictors=[TAirRoom_pred, power_hp_wb]
     )
 
     online_data = system.run(controllers=(hhp_MPC,), duration=one_day * 1)
-    online_data.plot(plotter=mpc_plotter)
+    online_data.plot(plotter=mpc_solution_plotter, save_plot=True, save_name=f'reference_{repetition}.png')
 
-    # online learning TAirRoom
-    TAirRoom_TrainingData.add(online_data)
-    TAirRoom_TrainingData.shuffle()
-    TAirRoom_TrainingData.split(0.7, 0.15, 0.15)
-    TAirRoom_pred.fit(
-        training_data=TAirRoom_TrainingData,
-        epochs=100,
-        batch_size=50,
-        verbose=1,
-    )
-    TAirRoom_pred.test(training_data=TAirRoom_TrainingData, show_plot=True)
-    power_hp_TrainingData.add(online_data)
-    power_hp_TrainingData.shuffle()
-    power_hp_TrainingData.split(0.8, 0, 0.2)
-    power_hp_gpr.fit(training_data=power_hp_TrainingData)
-    power_hp_gpr.test(training_data=power_hp_TrainingData)
+    # # online learning TAirRoom
+    # TAirRoom_TrainingData.add(online_data)
+    # TAirRoom_TrainingData.shuffle()
+    # TAirRoom_TrainingData.split(0.7, 0.15, 0.15)
+    # TAirRoom_pred.fit(
+    #     training_data=TAirRoom_TrainingData,
+    #     epochs=100,
+    #     batch_size=50,
+    #     verbose=1,
+    # )
+    # TAirRoom_pred.test(training_data=TAirRoom_TrainingData, show_plot=True)
+    # power_hp_TrainingData.add(online_data)
+    # power_hp_TrainingData.shuffle()
+    # power_hp_TrainingData.split(0.8, 0, 0.2)
+    # power_hp_gpr.fit(training_data=power_hp_TrainingData)
+    # power_hp_gpr.test(training_data=power_hp_TrainingData)
 
-    dh.save("mpc_linear_cost", override=True)
+    if df is None:
+        df = online_data.df
+    else:
+        df = pd.concat([df, online_data.df], axis=0)
+
+df.to_csv(str(Path(FileManager.experiment_dir(), 'data.csv')))
+
+kpis = system.get_kpis()
+kpis.to_csv(str(Path(FileManager.experiment_dir(), 'kpis.csv')), index=False)
+
--- Examples/BopTest/bestest_hydronic_heat_pump/configuration.py
+++ Examples/BopTest/bestest_hydronic_heat_pump/configuration.py
@@ -1,12 +1,19 @@
 from ddmpc import *
 
+time_offset = 1546300800
+
+TAirRoom_steady = Steady(day_start=8, day_end=15, day_target=290.15, night_target=294.15)
+TAirRoom_random = Random(day_start=8, day_end=19, day_lb=288.15, day_ub=303.15, night_lb=294.15, night_ub=297.15,
+                         interval=3600 * 6)
+TAirRoom_economic = Economic(day_start=8, day_end=19, day_lb=288.15, day_ub=303.15, night_lb=294.15, night_ub=297.15)
+
 TAirRoom = Controlled(
     source=Readable(
         name="TAir",
         read_name="reaTZon_y",
         plt_opts=PlotOptions(color=red, line=line_solid),
     ),
-    mode=Identification(),
+    mode=TAirRoom_steady,
 )
 
 TAirRoom_change = Connection(Change(base=TAirRoom))
@@ -95,10 +102,10 @@ price_el = Disturbance(
 costs_el = Connection(Product(b1=price_el, b2=power_hp, scale=0.001))
 
 def logistic(x):
-    return 1 / (1 + np.exp(-(x - 0.05) * 200))
+    return 1 / (1 + np.exp(-(x - 0.01) * 500))
 
 
-u_hp_logistic = Connection(Func(base=u_hp, func=logistic, name="logistic_154"))
+u_hp_logistic = Connection(Func(base=u_hp, func=logistic, name="logistic"))
 
 model = Model(*Feature.all)
 
@@ -106,6 +113,7 @@ system = BopTest(
     model=model,
     step_size=one_minute * 15,
     url="http://127.0.0.1:5000",
+    time_offset=time_offset,
 )
 
 power_hp_TrainingData = TrainingData(
@@ -113,7 +121,6 @@ power_hp_TrainingData = TrainingData(
         Input(source=u_hp, lag=1),
         Input(source=u_hp_logistic, lag=1),
         Input(source=t_amb, lag=1),
-        Input(source=TAirRoom_change, lag=1),
         Input(source=TAirRoom, lag=1),
     ),
     output=Output(power_hp),
@@ -122,10 +129,10 @@ power_hp_TrainingData = TrainingData(
 
 TAirRoom_TrainingData = TrainingData(
     inputs=Inputs(
-        Input(source=TAirRoom, lag=2),
-        Input(source=t_amb, lag=1),
+        Input(source=TAirRoom, lag=3),
+        Input(source=t_amb, lag=2),
         Input(source=rad_dir, lag=1),
-        Input(source=u_hp, lag=2),
+        Input(source=u_hp, lag=3),
     ),
     output=Output(TAirRoom_change),
     step_size=one_minute * 15,
