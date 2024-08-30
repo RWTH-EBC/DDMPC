0ec12ac4870238b57c8dcc48de5705c1730b18a9
ross.simon <simon.ross@eonerc.rwth-aachen.de>
2024-08-30 10:24:59 +0200

    modularize script: separate training data handling

--- Examples/BopTest/bestest_hydronic_heat_pump/s3_TAirRoom_ANN.py
+++ Examples/BopTest/bestest_hydronic_heat_pump/s3_TAirRoom_ANN.py
@@ -8,21 +8,10 @@ Train an ANN to learn the Temperature change using the generated training data
 
 def run(training_data_name: str, name: str, training_data: TrainingData):
 
-    # load DataHandler from pickle file saved in 2_generate_data
-    pid_data = load_DataHandler(f'{training_data_name}')
-
-    # add training data to Training Data object instantiated in configuration
-    # shuffle data and split into training, validation and testing sets
-    # write data into pickle file (same directory as pid_data file: /stored_data/data/ )
-    training_data.add(pid_data)
-    training_data.shuffle()
-    training_data.split(trainShare=0.8, validShare=0.1, testShare=0.1)
-    write_pkl(training_data, f'TrainingData_{name}_ANN', FileManager.data_dir())
-
     # Create a sequential Tuner Model for hyperparameter tuning
     tuner = TunerModel(
-        TunerBatchNormalizing(),            # layer to normalize inputs
-        TunerDense(units=(4, 8, 16)),              # layer can either have 4, 8 or 16 neurons
+        TunerBatchNormalizing(),  # layer to normalize inputs
+        TunerDense(units=(4, 8, 16)),  # layer can either have 4, 8 or 16 neurons
         # TunerDense(units=(4, 8), optional=True),
         name=name
     )
@@ -31,22 +20,51 @@ def run(training_data_name: str, name: str, training_data: TrainingData):
     trainer = NetworkTrainer()
     trainer.build(n=1, keras_tuner=tuner)
 
-    # train all neural networks build above by passing training data and training parameters
-    # print the configuration of the best network
-    # evaluate the trained neural networks (printing, saving and plotting evaluation by default False)
-    trainer.fit(
+    # load DataHandler from pickle file saved in 2_generate_data
+    pid_data = load_DataHandler(f'{training_data_name}')
+
+    training_data, trainer = handle_training_data(
         training_data=training_data,
-        epochs=1000,
-        batch_size=100,                 # number of test samples propagated through the network at once
-        verbose=1,                      # defines how the progress of the training is shown in terminal window
-        callbacks=[EarlyStopping(patience=100, verbose=1, restore_best_weights=True)]
-    )
+        data=pid_data,
+        split={'trainShare': 0.8, 'validShare': 0.1, 'testShare': 0.1},
+        trainer=trainer)
+    # write data into pickle file (same directory as pid_data file: /stored_data/data/ )
+    write_pkl(training_data, f'TrainingData_{name}_ANN', FileManager.data_dir())
+
+    # print the configuration of the best trained network
+    # evaluate the trained neural networks (printing, saving and plotting evaluation by default False)
     trainer.best.sequential.summary()
     trainer.eval(training_data=training_data, show_plot=True)
 
     # Saves all neural networks to pickle (directory: /stored_data/predictors/ )
     trainer.save(filename=f'{name}_ANN', override=True)
 
+
+def handle_training_data(training_data: TrainingData, data: DataHandler, split: dict, trainer: NetworkTrainer) -> [TrainingData, NetworkTrainer]:
+    """
+    add data to TrainingData object, shuffle and split training_data then fit trainer
+
+    :param training_data: TrainingData object
+    :param data: data to add to the TrainingData object
+    :param split: dict in the form {'trainShare': 0.8, 'validShare': 0.1, 'testShare': 0.1}
+    :param trainer: NetworkTrainer object
+    """
+
+    # add data to Training Data object
+    # shuffle data and split into training, validation and testing sets
+    training_data.add(data)
+    training_data.shuffle()
+    training_data.split(split['trainShare'], split['validShare'], split['testShare'])
+
+    # train all neural networks build above by passing training data and training parameters
+    trainer.fit(
+        training_data=training_data,
+        epochs=1000,
+        batch_size=100,  # number of test samples propagated through the network at once
+        verbose=1,  # defines how the progress of the training is shown in terminal window
+        callbacks=[EarlyStopping(patience=100, verbose=1, restore_best_weights=True)]
+    )
+    return training_data, trainer
 
 if __name__ == '__main__':
 
