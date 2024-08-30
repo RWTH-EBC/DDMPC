45a1f5937b99454b954f0b8240eed539556dc4ff
SiAndRo2002 <simon.andreas.ross@rwth-aachen.de>
2024-08-30 17:23:07 +0200

    Added online learning for GPR and linReg

--- Examples/BopTest/bestest_hydronic_heat_pump/online_learning.py
+++ Examples/BopTest/bestest_hydronic_heat_pump/online_learning.py
@@ -1,4 +1,6 @@
-import s3_TAirRoom_ANN as train_ann
+import s3_TAirRoom_ANN as train_ANN
+import s3_TAirRoom_GPR as train_GPR
+import s3_TAirRoom_linReg as train_linReg
 from typing import Optional
 from ddmpc.modeling.process_models.machine_learning import *
 from ddmpc.data_handling.storing_data import *
@@ -16,19 +18,38 @@ def online_learning(data: DataContainer, predictor: NeuralNetwork | LinearRegres
     if isinstance(predictor, NeuralNetwork):
         if not split:
             split = {'trainShare': 0.7, 'validShare': 0.15, 'testShare': 0.15}
-        training_data, predictor = train_ann.handle_training_data(
+
+        training_data, predictor = train_ANN.handle_training_data(
             training_data=predictor.training_data,
             data=data,
             split=split,
             trainer=predictor,
             **training_arguments
         )
-        predictor.test(training_data, show_plot=show_plot)
+
     elif isinstance(predictor, GaussianProcess):
         if not split:
             split = {'trainShare': 0.8, 'validShare': 0, 'testShare': 0.2}
+
+        training_data, predictor = train_GPR.handle_training_data(
+            training_data=predictor.training_data,
+            data=data,
+            split=split,
+            trainer=predictor,
+        )
+
     elif isinstance(predictor, LinearRegression):
         if not split:
             split = {'trainShare': 1.0, 'validShare': 0, 'testShare': 0}
+
+        training_data, predictor = train_linReg.handle_training_data(
+            training_data=predictor.training_data,
+            data=data,
+            split=split,
+            trainer=predictor,
+        )
+
     else:
         raise TypeError('predictor has to be of type NeuralNetwork, GaussianProcess or LinearRegression')
+
+    predictor.test(training_data, show_plot=show_plot)
